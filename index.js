8682590d8c4b852592f66fb63fedace1080e0af7
aarondodds1995 <azzadodds@gmail.com>
2026-05-15 22:19:41 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+
+import BankAccount from './bankAccount.js';
+
+const createBtn = document.getElementById("createAccount");
+const depositBtn = document.getElementById("depositBtn");
+const withdrawBtn = document.getElementById("withdrawBtn");
+const balanceBtn = document.getElementById("balanceBtn");
+
+const holderInput = document.getElementById("holderName");
+const balanceInput = document.getElementById("initialBalance");
+const accountNumberInput = document.getElementById("accountNumber");
+const amountInput = document.getElementById("amount");
+const output = document.getElementById("output");
