6a234ca440b12c3b0f020fb9c7f8e04388e2b3a1
aarondodds1995 <azzadodds@gmail.com>
2026-05-18 11:40:58 +0100

    bankAccount.js

--- bankAccount.js
+++ bankAccount.js
@@ -1,1 +1,10 @@
+// bankAccount.js
 
+export default class BankAccount {
+    #balance;
+
+    constructor(accountHolder, initialBalance = 0) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = this.generateAccountNumber();
+        this.#balance = initialBalance;
+    }
