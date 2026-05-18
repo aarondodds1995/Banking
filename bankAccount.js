c581df94c525e734e907f0609414284c72b16c01
aarondodds1995 <azzadodds@gmail.com>
2026-05-18 13:48:25 +0100

    bankAccount.js

--- bankAccount.js
+++ bankAccount.js
@@ -8,3 +8,35 @@ export default class BankAccount {
         this.accountNumber = this.generateAccountNumber();
         this.#balance = initialBalance;
     }
+
+    // Generate random 6-digit account number
+    generateAccountNumber() {
+        return Math.floor(100000 + Math.random() * 900000);
+    }
+
+    deposit(amount) {
+        if (amount > 0) {
+            this.#balance += amount;
+            return `Deposited $${amount}`;
+        } else {
+            return "Invalid deposit amount";
+        }
+    }
+
+    withdraw(amount) {
+        if (amount <= 0) {
+            return "Invalid withdrawal amount";
+        }
+
+        if (amount > this.#balance) {
+            return "Insufficient funds";
+        }
+
+        this.#balance -= amount;
+        return `Withdrawn $${amount}`;
+    }
+
+    checkBalance() {
+        return this.#balance;
+    }
+}
